999eafd83f821fff56cafce3ff7c4d66f9cd778b
Fathur Rohman <fathur.rohman@kemlu.go.id>
2023-05-18 00:15:20 +0200

    update seeder

--- packages/server/src/database/seeds/main.seeder.ts
+++ packages/server/src/database/seeds/main.seeder.ts
@@ -12,6 +12,7 @@ import { CommentsFactory } from "../factories/comment.factory";
 import { LikesFactory } from "../factories/like.factory";
 import { NotificationsFactory } from "../factories/notification.factory";
 import { faker } from "@faker-js/faker";
+import _ from "lodash";
 
 export default class MainSeeder implements Seeder {
   public async run(
@@ -21,22 +22,53 @@ export default class MainSeeder implements Seeder {
     // Run the factories here
 
     const postsRepository = dataSource.getRepository(Post);
+    const commentsRepository = dataSource.getRepository(Comment);
 
     const userFactory = factoryManager.get(User);
     const postsFactory = factoryManager.get(Post);
+    const commentFactory = factoryManager.get(Comment);
 
-    const users = await userFactory.saveMany(7);
+    const users = await userFactory.saveMany(25);
+
+    const numberOfComments = Math.floor(Math.random() * 100) + 1;
+
+    // const getMultipleRandomComments = (comments: Comment[], num: number) => {
+    //   const shuffled = [...comments].sort(() => 0.5 - Math.random());
+    //   return shuffled.slice(0, num);
+    // };
+
+    // const maxNumOfComments = Math.floor(Math.random() * 10) + 1;
+    // console.log(maxNumOfComments);
+    // let randomComment = _.sampleSize(comments, maxNumOfComments);
+    // let randomComment = getMultipleRandomComments(
+    //   comments,
+    //   maxNumOfComments
+    // );
 
     const posts = await Promise.all(
-      Array(17)
+      Array(100)
         .fill("")
         .map(async () => {
           const made = await postsFactory.make({
             author: faker.helpers.arrayElement(users),
+            //comments: randomComment,
           });
           return made;
         })
     );
     await postsRepository.save(posts);
+
+    const comments: Comment[] = await Promise.all(
+      Array(numberOfComments)
+        .fill("")
+        .map(async () => {
+          const made = await commentFactory.make({
+            author: faker.helpers.arrayElement(users),
+            post: faker.helpers.arrayElement(posts),
+          });
+          return made;
+        })
+    );
+    await commentsRepository.save(comments);
   }
 }
