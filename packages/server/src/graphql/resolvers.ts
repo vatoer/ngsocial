f8e0d20b80cecbf69518bb08ce44ba5cf17d5889
Fathur Rohman <fathur.rohman@kemlu.go.id>
2023-05-18 10:54:07 +0200

    set orm Context to working with Apolloserver using expressMiddleware

--- packages/server/src/graphql/resolvers.ts
+++ packages/server/src/graphql/resolvers.ts
@@ -1,5 +1,19 @@
+import { Context } from "..";
+
+import {
+  Resolvers,
+  User,
+  Post,
+  Comment,
+  Like,
+  Notification,
+} from "@ngsocial/graphql";
+
+import { ApolloError } from "@apollo/client";
+
 import { IResolvers } from "@graphql-tools/utils";
-const resolvers: IResolvers = {
+import { from } from "@apollo/client";
+const resolvers: Resolvers = {
   Query: {
     message: () => "It works!",
   },
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,9 @@
 import express, { Application } from "express";
-import { ApolloServer } from "@apollo/server";
+import { ApolloServer, BaseContext } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
+
+import { Repository } from "typeorm";
+
 import cors from "cors";
 import { json } from "body-parser";
 import "reflect-metadata";
@@ -10,6 +13,18 @@ import { gql } from "@apollo/client/core";
 import { IResolvers } from "@graphql-tools/utils";
 
 import appSchema from "./graphql/schema";
+import { User, Post, Comment, Like, Notification } from "./entity";
+import { GraphQLSchema } from "graphql";
+
+export type Context = {
+  orm: {
+    userRepository: Repository<User>;
+    postRepository: Repository<Post>;
+    commentRepository: Repository<Comment>;
+    likeRepository: Repository<Like>;
+    notificationRepository: Repository<Notification>;
+  };
+};
 
 const DataSource = AppDataSource;
 DataSource.initialize()
@@ -25,12 +40,42 @@ async function startApolloServer() {
   const app: Application = express();
   app.use(cors());
 
-  const server: ApolloServer = new ApolloServer({ schema: appSchema });
+  const userRepository: Repository<User> = DataSource.getRepository(User);
+  const postRepository: Repository<Post> = DataSource.getRepository(Post);
+  const commentRepository: Repository<Comment> =
+    DataSource.getRepository(Comment);
+  const likeRepository: Repository<Like> = DataSource.getRepository(Like);
+  const notificationRepository: Repository<Notification> =
+    DataSource.getRepository(Notification);
+
+  const context: Context = {
+    orm: {
+      userRepository: userRepository,
+      postRepository: postRepository,
+      commentRepository: commentRepository,
+      likeRepository: likeRepository,
+      notificationRepository: notificationRepository,
+    },
+  };
+
+  // https://www.apollographql.com/docs/apollo-server/migration/#context-initialization-function
+
+  const server = new ApolloServer<Context>({
+    schema: appSchema,
+  });
 
   await server.start();
   server.assertStarted("expressMiddleware()");
 
-  app.use("/graphql", json(), expressMiddleware(server));
+  app.use(
+    "/graphql",
+    json(),
+    expressMiddleware(server, {
+      context: async () => ({
+        orm: context.orm,
+      }),
+    })
+  );
 
   app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
